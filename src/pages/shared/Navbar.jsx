41369996e419f65366e3cd2a01f8e18f7c769eab
Sazzad Hossain <developersazzad@gmail.com>
2025-03-19 19:34:31 +0600

    home page completed

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,19 +1,13 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const [scrolling, setScrolling] = useState(false);
     const navOptions = <>
-        <li><a>Item 1</a></li>
-        <li>
-            <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                    <li><a>Submenu 1</a></li>
-                    <li><a>Submenu 2</a></li>
-                </ul>
-            </details>
-        </li>
-        <li><a>Item 3</a></li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/'>Home</Link></li>
+       
     </>
 
 
@@ -32,28 +26,31 @@ const Navbar = () => {
 
 
     return (
-        <section className={`navbar fixed top-0 max-w-7xl mx-auto z-10 transition-all duration-300 ${scrolling ? 'bg-blue-300 text-white' : 'text-white md:py-8'}`}>
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-                        </div>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            {navOptions}
-                        </ul>
+        <section className={`navbar fixed top-0 max-w-7xl mx-auto z-10 transition-all duration-300 ${scrolling ? 'bg-orange-400 text-white' : 'text-white md:py-8'}`}>
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-lg dropdown-content bg-orange-400 rounded-box z-1 mt-3 w-60 p-4 shadow">
                         {navOptions}
                     </ul>
                 </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
-                </div>
+                <Link to='/'>
+                    <h1 className="text-2xl font-bold">BistroBoss</h1>
+                    <p className="text-lg">Restaurents</p>
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-xl">
+                    {navOptions}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
         </section>
     );
 };
